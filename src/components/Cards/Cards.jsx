f3fbafcab2996dc8aa0f9cb1207ad45f03d66047
drexel-ue <packappsforyou@gmail.com>
2020-07-18 22:08:35 -0400

    Display fetched information

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-export default (props) => {
-    console.log('Cards Props', props);
+import styles from './Cards.module.css';
+
+export default ({ data: { confirmed, recovered, deaths } }) => {
+    if (!confirmed) return 'Loading...';
 
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5'>{confirmed.value}</Typography>
+                        <Typography color='textSecondary'>DATE</Typography>
+                        <Typography variant='body2'>Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+                        <Typography variant='h5'>{recovered.value}</Typography>
+                        <Typography color='textSecondary'>DATE</Typography>
+                        <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+                        <Typography variant='h5'>{deaths.value}</Typography>
+                        <Typography color='textSecondary'>DATE</Typography>
+                        <Typography variant='body2'>Number of deaths due to COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div >
     );
+
 }
