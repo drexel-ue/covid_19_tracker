47d18bd0f5a5affccf6d253c23bfabfa33d588e4
drexel-ue <packappsforyou@gmail.com>
2020-07-18 23:24:05 -0400

    Show line chart or world infected vs death rates

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+import styles from './Chart.module.css';
+
 
 export default () => {
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetch = async () => {
+            setDailyData(await fetchDailyData());
+        };
+
+        console.log('Chart Data', dailyData);
+
+        fetch();
+    }, [dailyData.length]);
+
+    const lineChart = (
+        dailyData[0] ? <Line data={{
+            labels: dailyData.map(({ reportDate }) => reportDate),
+            datasets: [
+                {
+                    data: dailyData.map(({ confirmed }) => confirmed.total),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+                },
+                {
+                    data: dailyData.map(({ deaths }) => deaths.total),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(225, 0, 0, .5)',
+                    fill: true,
+                },
+            ],
+        }} /> : null
+    );
+
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     );
 }
