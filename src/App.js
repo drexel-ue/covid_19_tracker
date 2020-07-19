005b8670660c1e606d56ca59a384f824c8c29308
drexel-ue <packappsforyou@gmail.com>
2020-07-18 21:31:54 -0400

    Future setup of  project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+
+import { Cards, Chart, CountryPicker } from "./components/index";
+import styles from "./App.module.css";
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
